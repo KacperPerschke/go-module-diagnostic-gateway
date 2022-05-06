1d9ff684b31ff76317fd017208cdf4c9b209a981
KAcper Perschke <KAcper.Perschke@gmail.com>
2022-05-06 08:19:32 +0200

    Taken from https://gist.github.com/arunvelsriram/74fa35d1c9e6bbf94b8f21b42a5f07e1

--- main.go
+++ main.go
@@ -1,129 +1,85 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"fmt"
-	"log"
 	"net/http"
-	"os"
-	"os/signal"
-	"sync/atomic"
 	"time"
-)
 
-type key int
-
-const (
-	requestIDKey key = 0
-)
-
-var (
-	listenAddr string
-	healthy    int32
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	flag.StringVar(&listenAddr, "listen-addr", ":5000", "server listen address")
+	var listenPort string
+
+	flag.StringVar(&listenPort, "listen-addr", "5000", "server listen address")
 	flag.Parse()
 
-	logger := log.New(os.Stdout, "http: ", log.LstdFlags)
-	logger.Println("Server is starting...")
+	http.Handle("/ping", WithLogging(pingHandler()))
 
-	router := http.NewServeMux()
-	router.Handle("/", index())
-	router.Handle("/healthz", healthz())
+	addr := fmt.Sprintf("127.0.0.1:%s", listenPort)
+	logrus.WithField("addr", addr).Info("starting server")
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		logrus.WithField("event", "start server").Fatal(err)
+	}
+}
 
-	nextRequestID := func() string {
-		return fmt.Sprintf("%d", time.Now().UnixNano())
+func pingHandler() http.Handler {
+	fn := func(rw http.ResponseWriter, r *http.Request) {
+		rw.WriteHeader(http.StatusOK)
+		_, _ = fmt.Fprintf(rw, "pong")
+	}
+	return http.HandlerFunc(fn)
+}
+
+type (
+	// struct for holding response details
+	responseData struct {
+		status int
+		size   int
 	}
 
-	server := &http.Server{
-		Addr:         listenAddr,
-		Handler:      tracing(nextRequestID)(logging(logger)(router)),
-		ErrorLog:     logger,
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 10 * time.Second,
-		IdleTimeout:  15 * time.Second,
+	// our http.ResponseWriter implementation
+	loggingResponseWriter struct {
+		http.ResponseWriter // compose original http.ResponseWriter
+		responseData        *responseData
 	}
+)
 
-	done := make(chan bool)
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, os.Interrupt)
+func (r *loggingResponseWriter) Write(b []byte) (int, error) {
+	size, err := r.ResponseWriter.Write(b) // write response using original http.ResponseWriter
+	r.responseData.size += size            // capture size
+	return size, err
+}
 
-	go func() {
-		<-quit
-		logger.Println("Server is shutting down...")
-		atomic.StoreInt32(&healthy, 0)
+func (r *loggingResponseWriter) WriteHeader(statusCode int) {
+	r.ResponseWriter.WriteHeader(statusCode) // write status code using original http.ResponseWriter
+	r.responseData.status = statusCode       // capture status code
+}
 
-		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
-		defer cancel()
+func WithLogging(h http.Handler) http.Handler {
+	loggingFn := func(rw http.ResponseWriter, req *http.Request) {
+		start := time.Now()
 
-		server.SetKeepAlivesEnabled(false)
-		if err := server.Shutdown(ctx); err != nil {
-			logger.Fatalf("Could not gracefully shutdown the server: %v\n", err)
+		responseData := &responseData{
+			status: 0,
+			size:   0,
 		}
-		close(done)
-	}()
-
-	logger.Println("Server is ready to handle requests at", listenAddr)
-	atomic.StoreInt32(&healthy, 1)
-	if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-		logger.Fatalf("Could not listen on %s: %v\n", listenAddr, err)
-	}
-
-	<-done
-	logger.Println("Server stopped")
-}
-
-func index() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/" {
-			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
-			return
+		lrw := loggingResponseWriter{
+			ResponseWriter: rw, // compose original http.ResponseWriter
+			responseData:   responseData,
 		}
-		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-		w.Header().Set("X-Content-Type-Options", "nosniff")
-		w.WriteHeader(http.StatusOK)
-		fmt.Fprintln(w, "Hello, World!")
-	})
-}
+		h.ServeHTTP(&lrw, req) // inject our implementation of http.ResponseWriter
 
-func healthz() http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if atomic.LoadInt32(&healthy) == 1 {
-			w.WriteHeader(http.StatusNoContent)
-			return
-		}
-		w.WriteHeader(http.StatusServiceUnavailable)
-	})
-}
+		duration := time.Since(start)
 
-func logging(logger *log.Logger) func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			defer func() {
-				requestID, ok := r.Context().Value(requestIDKey).(string)
-				if !ok {
-					requestID = "unknown"
-				}
-				logger.Println(requestID, r.Method, r.URL.Path, r.RemoteAddr, r.UserAgent())
-			}()
-			next.ServeHTTP(w, r)
-		})
+		logrus.WithFields(logrus.Fields{
+			"uri":      req.RequestURI,
+			"method":   req.Method,
+			"status":   responseData.status,
+			"duration": duration,
+			"size":     responseData.size,
+		}).Info("request completed")
 	}
-}
-
-func tracing(nextRequestID func() string) func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			requestID := r.Header.Get("X-Request-Id")
-			if requestID == "" {
-				requestID = nextRequestID()
-			}
-			ctx := context.WithValue(r.Context(), requestIDKey, requestID)
-			w.Header().Set("X-Request-Id", requestID)
-			next.ServeHTTP(w, r.WithContext(ctx))
-		})
-	}
+	return http.HandlerFunc(loggingFn)
 }
